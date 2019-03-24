faac1f8c8034a06e55b4bd12cd356efb1488823e
Ilya Rozhkovsky <poiskull@gmail.com>
2019-03-24 19:58:12 +0300

    feat: add money exchanger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) return {};
+
+    else if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    else {
+        let split = new Object();
+
+        split.H = Math.floor(currency / 50);
+        split.Q = Math.floor((currency - split.H * 50) / 25);
+        split.D = Math.floor((currency - split.H * 50 - split.Q * 25) / 10);
+        split.N = Math.floor((currency - split.H * 50 - split.Q * 25 - split.D * 10) / 5);
+        split.P = Math.floor(currency % 5);
+
+        for (key in split) {
+
+            if (split[key] < 1) delete split[key];
+
+        }
+        return split;
+    }
 }
